98282a91f136e9813af6070470e0579235dc357b
Shadowbeetle <tamas.kadlecsik@gmail.com>
2022-05-18 15:10:40 +0200

    add Article component

--- web/src/components/ArticlesCell/ArticlesCell.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -1,6 +1,8 @@
 import type { ArticlesQuery } from 'types/graphql'
+
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 import { Link, routes } from '@redwoodjs/router'
+import Article from '../Article/Article'
 
 export const QUERY = gql`
   query ArticlesQuery {
@@ -25,15 +27,7 @@ export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
   return (
     <ul>
       {articles.map((article) => (
-        <article key={article.id}>
-          <header>
-            <Link to={routes.article({ id: article.id })}>
-              {article.title}
-            </Link>
-          </header>
-          <p>{article.body}</p>
-          <div>Posted at: {article.createdAt}</div>
-        </article>
+        <Article key={article.id} article={article} />
       ))}
     </ul>
   )
